c69455d4e400d191a4ef17a7c37d324992009987
GregoryBaranov <mike25012000@gmail.com>
2019-04-05 09:19:24 +0300

    add web

---
+++ stream-chat/script.js
@@ -0,0 +1,83 @@
+var xhr = new XMLHttpRequest();
+
+        setInterval(sdad, 500)
+        setInterval(gett, 500)
+
+        var a = "";
+
+
+        function gett() {
+            if (xhr.responseText != " " && xhr.responseText != a && xhr.responseText != "") {
+                output(xhr.responseText);
+                a = xhr.responseText;
+            }
+        }
+
+        function sdad() {
+            xhr.open('GET', 'http://176.195.178.253/ismessage', false);
+            xhr.send();
+        }
+
+        function Send() {
+
+            var input = document.getElementById("input");
+            var text = input.value;
+
+            if (text != " " && text != "") {
+                xhr.open('GET', 'http://176.195.178.253/sendMessage?text=' + text, false);
+                xhr.send();
+            }
+            document.getElementById('input').value = "";
+        }
+
+        function output(message) {
+            var output = document.getElementById("output");
+            output.value += "Пользователь написал: " + message + "\n";
+            if (output.selectionStart == output.selectionEnd) {
+                output.scrollTop = output.scrollHeight;
+            }
+
+
+        }
+
+
+
+        $(document).ready(function() {
+
+
+            $("#output").change(function() {
+                scrollToBottom();
+            });
+
+
+            $('input').keydown(function(e) {
+                if (e.keyCode === 13) {
+                    Send();
+                }
+            });
+
+
+            $('#temp').hide();
+
+            function scrollToBottom() {
+                $('#output').scrollTop($('#output')[0].scrollHeight);
+            }
+
+
+            $('.btn-hide').on('click', function() {
+                $('#cont1').slideToggle(700);
+                $('#temp').show();
+
+            });
+
+
+            $('#temp').on('click', function() {
+                $('#cont1').slideToggle(700);
+
+                $('#temp').hide();
+
+
+            });
+
+
+        });

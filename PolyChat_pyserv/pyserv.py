4fbf33c4e7f95b36b288709eac21a9d0b420c405
GregoryBaranov <mike25012000@gmail.com>
2019-04-08 00:21:25 +0300

    remove branches and rename folders

---
+++ PolyChat_pyserv/pyserv.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+application = app
+
+a = ""
+
+temp = []
+
+
+@app.route('/')
+def default():
+    return ("index")
+
+
+@app.route('/sendMessage')
+def sendMessage():
+    token = request.args.get('token', '')
+    ms_text = request.args.get('text', '')
+    hash = request.args.get('hash', '')
+    global a
+    a = request.args.get('text', '')
+    temp.append(a)
+    return (a)
+
+
+@app.route('/ismessage')
+def ismessage():
+    return (a)
+
+
+#if __name__ == '__main__':
+    #app.Debug = True
+    #app.run('0.0.0.0', 5007)

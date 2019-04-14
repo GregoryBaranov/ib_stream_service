08c5c679331177d4d286efd555e005884fe32f09
GregoryBaranov <mike25012000@gmail.com>
2019-04-14 18:06:24 +0300

    add utf-8

--- tempchat/main.py
+++ tempchat/main.py
@@ -17,9 +17,29 @@ def messageReceived(methods=['GET', 'POST']):
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+#    data = {'message': json['message'].encode('latin1').decode('utf8','replace')}
+#    socketio.emit('my response', data, callback=messageReceived)
+    
+    
+    a = str(json.setdefault('message')).encode('latin1').decode('utf8','replace')
+    print(a)
+    if a == "None":
+        b = dict.fromkeys(['message'], 'Welcome')
+    else:
+        b = {}
+        b = dict.fromkeys(['message'], a)
+        print(b)
+        socketio.emit('my response', b,  callback=messageReceived)
+    
+#    a = str(json.keys()).encode('latin1').decode('unicode-escape').encode('latin1').decode('utf8','replace')
+#    print(json)
+##    b = dict.fromkeys(['data'])
+#    b = dict.fromkeys([a])
+#    print("это a - " , a)
+#    print("это json - " , json)
+#    print("это Б - " , b)
+    
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, '0.0.0.0', 5007)

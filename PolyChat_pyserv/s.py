a165a8b8eb49d94b81e046d13a9cb841b4df7261
Facassanxt <asteblo@gmail.com>
2019-06-03 04:26:12 +0300

    Configured connection with the application

--- PolyChat_pyserv/s.py
+++ PolyChat_pyserv/s.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
 from flask_socketio import SocketIO
 from flask_sockets import Sockets
 import websockets
 import threading
 import json
+import socket
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -11,8 +13,22 @@ socketio = SocketIO(app)
 sockets = Sockets(app)
 application = app
 
-data = "hello world"
+def transform(text_file_contents):
+    return text_file_contents.replace("=", ",")
+@app.route('/transform', methods=["POST"])
+def transform_view():
+    file = request.files['data_file']
+    if not file:
+        return "No file"
 
+    file_contents = file.stream.read().decode("utf-8","ignore")
+
+    result = transform(file_contents)
+
+    response = make_response(result)
+    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+    return response
+	
 @app.route('/')
 def sessions():
     return render_template('session.html')
@@ -22,30 +38,43 @@ def messageReceived(methods=['GET', 'POST']):
 	
 @sockets.route('/')
 def echo_socket(ws):
+    print(ws)
+    list.append(ws)
     while not ws.closed:
-        print(ws)
         message = ws.receive()
-        ws.send(data)
-        prin(data)
         ws.send(message)
         print(message)
         data = {'id': 'streamer','message': message}
         socketio.emit('my response', data)
+
+        if message == "Disconnect":
+            print("=====================================")
+            print(list.remove(ws))
+            print(list)
+            print("=====================================")
+            ws.close()
+    if ws.closed:
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
 		
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print(json)
     data = {'id': json['id'].encode().decode('utf8','replace'),'message': json['message'].encode().decode('utf8','replace').replace('<', '&lt')}
+    print(json['message'].encode().decode('utf8','replace').replace('<', '&lt'))
     socketio.emit('my response', data, callback=messageReceived)
-	
-@app.route('/echo_test', methods=['GET'])
-def echo_test():
-    return render_template('echo_test.html')
-
-		
+    print(list)
+    print(len(list))
+    i = 0
+    while i < len(list):
+        ws = list[i]
+        ws.send(json['message'].encode().decode('utf8','replace').replace('<', '&lt'))
+        i = i + 1
 
 if __name__ == "__main__":
+    global list
+    list = []
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('0.0.0.0', 5007), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
     server.serve_forever()

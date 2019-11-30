44ec2c103c72602d0407bd1caf0ba39315740a5d
Алексей Стебло <asteblo@gmail.com>
2019-11-30 19:53:45 +0300

    30.11.19

--- FreePolyCall_2019/PolyChat_pyserv/app.py
+++ FreePolyCall_2019/PolyChat_pyserv/app.py
@@ -5,14 +5,13 @@ import websockets
 import threading
 import json
 import socket
+import random
 
 import math, re, sys
 #from werkzeug.contrib.cache import MemcachedCache      
 #from __init__ import buf
-#import redis
 #import uuid
 #from flask_caching import Cache
-
 app = Flask(__name__)
 app.debug = 1
 socketio = SocketIO(app)
@@ -21,24 +20,46 @@ application = app
 app.secret_key = "super secret key"
 
 
+# arr = ""
+#     for key in red1.keys():
+#         arr += "#{}${}&&".format(key.decode('utf8','replace'), red1.get(key).decode('utf8','replace'))
+
+
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.method == 'POST':
-        ses_id = request.form.get('ses_id', '')
-        if ses_id:
-            #flash('Пользователь с таким никнеймом уже существует!')
-            return redirect('/'+str(ses_id))
+        ses_id = str(request.form.get('ses_id', ''))
+        Email = request.form.get('Email', '')
+        Username = request.form.get('name_usr', '')
+        if ses_id and Email and Username:
+            print(Dict)
+            try:
+                if Dict[ses_id]:
+                    cookie = str(random.randint(1,999999))
+                    bd[ses_id].append({Username:cookie})
+                    resp = make_response(redirect(ses_id))
+                    resp.set_cookie('Cookie', cookie, max_age=60*60)
+                    return resp
+            except:
+                pass
     return render_template('index.html')
-
 
 @app.route('/<int:id_ses>')
 def qwe(id_ses):
-    return render_template('page2.html')
+    try:
+        if Dict[str(id_ses)]:
+            return render_template('page2.html')
+    except:
+        return redirect('/')
 
 @sockets.route('/<int:id_ses>') #Сокет для Qt приложения
 def echo_socket(ws,id_ses):  #Получение сокета
+    id_ses = str(id_ses)
+    bd.update({id_ses:[]})
+    print(bd)
     print(ws) #Проверка сокета
-    Dict.update({str(id_ses): ws})
+    Dict.update({id_ses: ws})
+    print(Dict)
     while not ws.closed: #Пока есть соединение
         message = ws.receive() #Считываем сообщение от QT
         ws.send('Streamer: ' + message) #Отправляем их обратно на QT
@@ -54,18 +75,48 @@ def echo_socket(ws,id_ses):  #Получение сокета
 @socketio.on('my event') #socketio на event'ах
 def handle_my_custom_event(json): #Получаем Json
     print(json)
-    if json['message'].encode().decode('utf8','replace').replace('<', '&lt') != "": #Если в Json есть собщение, то
+    flag = True
+    for i in bd[json['session_id']]:
+        for key,value in i.items():
+            if value == request.cookies.get('Cookie'):
+                if ignor:
+                    for i in ignor:
+                        if 'Алексей' == i:
+                            flag = False
+                else:
+                    flag = True
+            else:
+                flag = False
+            
+
+    if json['message'].encode().decode('utf8','replace').replace('<', '&lt') != "" and flag == True: #Если в Json есть собщение, то
         data = {'id': json['id'].encode().decode('utf8','replace'), #Исправляем кодировку
                 'message': json['message'].encode().decode('utf8','replace').replace('<', '&lt')} #Заменяем знак '<' на спецсимвол для защиты от html разметки 
         print(json['id'].encode().decode('utf8','replace') + json['message'].encode().decode('utf8','replace').replace('<', '&lt'))
         socketio.emit(json['session_id'], data) #Отправляем сообщение в браузер
         print(json['session_id'])
-    if Dict[json['session_id']]:   
+    if Dict[json['session_id']] and flag == True:   
         Dict[json['session_id']].send(json['id'].encode().decode('utf8','replace') + json['message'].encode().decode('utf8','replace').replace('<', '&lt')) #Отправляем QT стримеру сообщение
+    if json['message'].encode().decode('utf8','replace').replace('<', '&lt') == "test":
+        print(bd)
+        # for i in range(len(bd[json['session_id']])):
+        #     for l in bd[json['session_id']]:
+        #         for key,value in l.items():
+        #             if key == 'Username':
+        #                 bd[json['session_id']].pop(i)
+        for i in bd[json['session_id']]:
+            for key,value in i.items():
+                if key == 'Алексей':
+                    ignor.append(key)    
 
 if __name__ == "__main__":
     global Dict
     Dict = {}
+    global ignor
+    ignor = []
+    global bd
+    bd = {}
+
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
     server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler) #Настройка сервера
--- FreePolyCall_2019/PolyChat_pyserv/static/js/scriptus.js
+++ FreePolyCall_2019/PolyChat_pyserv/static/js/scriptus.js
@@ -1,21 +1,4 @@
  var socket = io.connect('http://' + document.domain + ':' + location.port);
-      $(document).ready(function () {
-        function scrollToBottom() {
-          $('#output').scrollTop($('#output')[0].scrollHeight);
-        }
-        $("#output").change(function () {
-          scrollToBottom();
-        });
-        $('#temp').hide();
-        $('.btn-hide').on('click', function () {
-          $('#cont1').slideToggle(700);
-          $('#temp').show();
-        });
-        $('#temp').on('click', function () {
-          $('#cont1').slideToggle(700);
-          $('#temp').fadeOut('slow');
-        });
-      });
       $("#as").click(function (e) {
         e.preventDefault()
         let user_input = $('input.message').val()
@@ -26,7 +9,7 @@
         });
         $('input.message').val('').focus()
       });
-	  
+    
   // 1. Создаём новый объект XMLHttpRequest
   var xhr = new XMLHttpRequest();
   // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
@@ -74,130 +57,3 @@
       }
     }
   })
-  window.location.pathname.substr(1)
-  $('div.panel-heading').append('<h3 class="panel-title">' + window.location.pathname.substr(1) + '</h3>')
-
-particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 133,
-        "density": {
-          "enable": true,
-          "value_area": 946.9771699587272
-        }
-      },
-      "color": {
-        "value": "#fff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.39457382081613634,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#fff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 73.08694910712106,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true
-  });
-  var stats, update;
-  stats = new Stats;
-  stats.setMode(0);
-  stats.domElement.style.position = 'absolute';
-  count_particles = document.querySelector('.js-count-particles');
-  update = function () {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);;

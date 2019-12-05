1759d20035e6d3452669c89f66f5a12233006655
Алексей Стебло <asteblo@gmail.com>
2019-12-05 04:33:59 +0300

    05.12.19

--- FreePolyCall_2019/PolyChat_pyserv/app.py
+++ FreePolyCall_2019/PolyChat_pyserv/app.py
@@ -32,11 +32,20 @@ def home():
         Email = request.form.get('Email', '')
         Username = request.form.get('name_usr', '')
         if ses_id and Email and Username:
-            print(Dict)
             try:
-                if Dict[ses_id]:
+                for i in bd[ses_id]:
+                    for key,value in i.items():
+                        if key == Username:
+                            flash("Ник занят")
+                            return render_template('index.html')
+            except:
+                pass
+            print(Stream)
+            try:
+                if Stream[ses_id]:
                     cookie = str(random.randint(1,999999))
                     bd[ses_id].append({Username:cookie})
+                    Stream[ses_id].send("%%%" + Username + "$$$") #Отправляем QT стримеру сообщение
                     resp = make_response(redirect(ses_id))
                     resp.set_cookie('Cookie', cookie, max_age=60*60)
                     return resp
@@ -47,29 +56,58 @@ def home():
 @app.route('/<int:id_ses>')
 def qwe(id_ses):
     try:
-        if Dict[str(id_ses)]:
+        if Stream[str(id_ses)]:
             return render_template('page2.html')
     except:
         return redirect('/')
 
 @sockets.route('/<int:id_ses>') #Сокет для Qt приложения
 def echo_socket(ws,id_ses):  #Получение сокета
+    ws.send('Pong')
     id_ses = str(id_ses)
-    bd.update({id_ses:[]})
+    # bd.update({id_ses:[]})
     print(bd)
     print(ws) #Проверка сокета
-    Dict.update({id_ses: ws})
-    print(Dict)
-    while not ws.closed: #Пока есть соединение
-        message = ws.receive() #Считываем сообщение от QT
-        ws.send('Streamer: ' + message) #Отправляем их обратно на QT
-        print(message)
-        data = {'id': 'streamer','message': message} #Записываем в Json
-        socketio.emit(id_ses, data) #Пересылаем сообщение от QT в браузер
-        if message == "Disconnect":
-            print(Dict) #Проверка пустоты
-            Dict.pop(str(id_ses))
-            ws.close() #Qt стример отключился
+    print(Stream)
+    flag = True
+    for i in bd:
+        if i == id_ses:
+            print('++++++++++++++')
+            ws.send('Сессия "' + id_ses +'" в данный момент занята!')
+            flag = False
+    if flag == True:
+        print('=============')
+        bd.update({id_ses:[]})
+        ignor.update({id_ses:[]})
+        print(bd)
+        print(ws) #Проверка сокета
+        Stream.update({id_ses: ws})
+        print(Stream)
+        while not ws.closed: #Пока есть соединение
+            message = ws.receive() #Считываем сообщение от QT
+            result = re.search(r'[%]{3}([a-zA-ZА-Яа-я]+)[&]{2}([\S]+)[$]{3}', message)
+            if result:
+                if result.group(1) == "MUTE":
+                    print('-'*32)
+                    print(result.group(2))
+                    print('-'*32)
+                    for i in bd[id_ses]:
+                        for key,value in i.items():
+                            if key == result.group(2):
+                                ignor[id_ses].append(key)
+                if result.group(1) == "UNMUTE":
+                    ignor[id_ses].remove(result.group(2))
+            else:
+                ws.send('Streamer: ' + message) #Отправляем их обратно на QT
+                print(message)
+                data = {'id': 'streamer','message': message} #Записываем в Json
+                socketio.emit(id_ses, data) #Пересылаем сообщение от QT в браузер
+                if message == "Disconnect":
+                    print("Сокеты стримера")
+                    print(Stream) #Проверка пустоты
+                    Stream.pop(id_ses)
+                    bd.pop(id_ses)
+                    ws.close() #Qt стример отключился
 
 
 @socketio.on('my event') #socketio на event'ах
@@ -78,43 +116,27 @@ def handle_my_custom_event(json): #Получаем Json
     flag = True
     for i in bd[json['session_id']]:
         for key,value in i.items():
-            if value == request.cookies.get('Cookie'):
-                if ignor:
-                    for i in ignor:
-                        if 'Алексей' == i:
+            if value == json['id']:
+                test = key
+                if ignor[json['session_id']]:
+                    for i in ignor[json['session_id']]:
+                        if test == i:
                             flag = False
-                else:
-                    flag = True
-            else:
-                flag = False
-            
 
     if json['message'].encode().decode('utf8','replace').replace('<', '&lt') != "" and flag == True: #Если в Json есть собщение, то
-        data = {'id': json['id'].encode().decode('utf8','replace'), #Исправляем кодировку
-                'message': json['message'].encode().decode('utf8','replace').replace('<', '&lt')} #Заменяем знак '<' на спецсимвол для защиты от html разметки 
+        data = {'id': test,'message': json['message'].encode().decode('utf8','replace').replace('<', '&lt')} #Заменяем знак '<' на спецсимвол для защиты от html разметки 
         print(json['id'].encode().decode('utf8','replace') + json['message'].encode().decode('utf8','replace').replace('<', '&lt'))
         socketio.emit(json['session_id'], data) #Отправляем сообщение в браузер
         print(json['session_id'])
-    if Dict[json['session_id']] and flag == True:   
-        Dict[json['session_id']].send(json['id'].encode().decode('utf8','replace') + json['message'].encode().decode('utf8','replace').replace('<', '&lt')) #Отправляем QT стримеру сообщение
-    if json['message'].encode().decode('utf8','replace').replace('<', '&lt') == "test":
         print(bd)
-        # for i in range(len(bd[json['session_id']])):
-        #     for l in bd[json['session_id']]:
-        #         for key,value in l.items():
-        #             if key == 'Username':
-        #                 bd[json['session_id']].pop(i)
-        for i in bd[json['session_id']]:
-            for key,value in i.items():
-                if key == 'Алексей':
-                    ignor.append(key)    
+        print(ignor)
+    if Stream[json['session_id']] and flag == True:   
+        Stream[json['session_id']].send(test + ": " + json['message'].encode().decode('utf8','replace').replace('<', '&lt')) #Отправляем QT стримеру сообщение
 
 if __name__ == "__main__":
-    global Dict
-    Dict = {}
-    global ignor
-    ignor = []
-    global bd
+    global Stream,ignor,bd
+    Stream = {}
+    ignor = {}
     bd = {}
 
     from gevent import pywsgi
--- FreePolyCall_2019/PolyChat_pyserv/static/js/scriptus.js
+++ FreePolyCall_2019/PolyChat_pyserv/static/js/scriptus.js
@@ -1,59 +1,63 @@
- var socket = io.connect('http://' + document.domain + ':' + location.port);
-      $("#as").click(function (e) {
-        e.preventDefault()
-        let user_input = $('input.message').val()
-        socket.emit('my event', {
-          id: a,
-          message: user_input,
-          session_id: window.location.pathname.substr(1)
-        });
-        $('input.message').val('').focus()
-      });
-    
-  // 1. Создаём новый объект XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-  xhr.open('GET', 'https://ip.up66.ru/', false);
-  // 3. Отсылаем запрос
-  xhr.send();
-  // 4. Если код ответа сервера не 200, то это ошибка
-  if (xhr.status != 200) {
-    // обработать ошибку
-    alert(xhr.status + ': ' + xhr.statusText); // пример вывода: 404: Not Found
-  } else {
-    // вывести результат
-    if (xhr.responseText == '93.185.192.87' || xhr.responseText == '213.87.159.169') {
-      a = "Тимур: ";
-    } else if (xhr.responseText == '176.195.178.253') {
-      a = "Григорий: ";
-    } else if (xhr.responseText == '31.10.65.179' || xhr.responseText == '94.25.170.254') {
-      a = "Алексей: ";
-    } else if (xhr.responseText == '77.232.17.21') {
-      a = "ПетУх";
+$('div.test').append('<a class="navbar-brand text-white"> Session\'s ID: ' + window.location.pathname.substr(1) + '</a>')
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+$("#as").click(function (e) {
+  e.preventDefault()
+  let user_input = $('input.message').val()
+  socket.emit('my event', {
+    id: $.cookie("Cookie"),
+    message: user_input,
+    session_id: window.location.pathname.substr(1)
+  });
+  $('input.message').val('').focus()
+});
+
+socket.on(window.location.pathname.substr(1), function (msg) {
+  console.log(msg)
+  console.log(Temp)
+  if (Temp == 1) {
+    $("#supa").hide();
+  }
+  if (Temp == 0) {
+    $("#supa").toggle();
+    $("#supa").show();
+  }
+  if (typeof msg.message !== 'undefined' && msg.message != '' && msg.message != ' ') {
+    var output = document.getElementById("output");
+    if (msg.id == 'streamer') {
+      $('div.message_holder').append(
+        '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-danger text-white rounded"  ><b style="color: #000;">' +
+        'Стример: ' + '</b>' + msg.message + '</div>')
     } else {
-      a = xhr.responseText + ': ';
+      $('div.message_holder').append(
+        '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-info text-white rounded"><b style="color: #000;">' +
+        msg.id + ': </b>' + msg.message + '</div>')
+    }
+
+    if (output.selectionStart == output.selectionEnd) {
+      output.scrollTop = output.scrollHeight;
     }
   }
-  socket.on(window.location.pathname.substr(1), function (msg) {
-    console.log(msg)
-    if (typeof msg.message !== 'undefined' && msg.message != '' && msg.message != ' ') {
-      var output = document.getElementById("output");
-      //                var newmsg = "<xmp style='color: red; font-size: 18px;font-family: 'Roboto', sans-serif;'>" + msg.message + "</xmp>"
-      if (a == msg.id) {
-        $('div.message_holder').append(
-          '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-info text-white shadow-lg rounded"><b style="color: #000;">' +
-          msg.id + '</b>' + msg.message + '</div>')
-      } else if (msg.id == 'streamer') {
-        $('div.message_holder').append(
-          '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-danger text-white shadow-lg rounded"  ><b style="color: #000;">' +
-          'Стример: ' + '</b>' + msg.message + '</div>')
-      } else {
-        $('div.message_holder').append(
-          '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-secondary text-white shadow-lg rounded"  ><b style="color: #000;">' +
-          msg.id + '</b>' + msg.message + '</div>')
-      }
-      if (output.selectionStart == output.selectionEnd) {
-        output.scrollTop = output.scrollHeight;
-      }
+})
+
+Temp = 0;
+$(document).ready(function () {
+  function scrollToBottom() {
+    $('#output').scrollTop($('#output')[0].scrollHeight);
+  }
+  $("#output").change(function () {
+    scrollToBottom();
+  });
+  $("#btn").click(function () {
+    $("#supa").hide();
+    $("#Create").toggle();
+    if (Temp == 0) {
+      Temp = 1;
+    } else {
+      Temp = 0;
     }
-  })
+    $('#output').scrollTop($('#output')[0].scrollHeight);
+  });
+});
+
+$("#supa").hide();
+$("#Create").hide();

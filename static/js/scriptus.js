15963a15b4d4bce15a1d167d96dd86c704eb29a1
Facassanxt <43217464+Facassanxt@users.noreply.github.com>
2020-04-12 22:15:37 +0300

    12.04.2020 Switching to sqlite3 completed by 90% and spent 6 hours,also an attempt to add Anti-DDoS Steblo And Zaxarov

---
+++ static/js/scriptus.js
@@ -0,0 +1,98 @@
+$('div.test').append('<a class="navbar-brand text-white"> Session\'s ID: ' + window.location.pathname.substr(1) + '</a>')
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+$("#as").click(function (e) {
+  e.preventDefault()
+  let user_input = $('input.message').val()
+  socket.emit('my event', {
+    id: $.cookie("Cookie"),
+    message: user_input,
+    session_id: window.location.pathname.substr(1)
+  });
+  $('input.message').val('').focus()
+});
+
+socket.on(window.location.pathname.substr(1), function (msg) {
+  console.log(msg)
+  console.log(Temp)
+  if (Temp == 1) {
+    $("#supa").hide();
+  }
+  if (Temp == 0) {
+    $("#btn").addClass('blink').html();
+    $("#supa2").hide();
+    $("#supa").toggle();
+    $("#supa").show();
+  }
+  if (typeof msg.title !== '' && msg.image != '' && msg.website != ' ') {
+    var output = document.getElementById("output");
+    if (msg.id == 'streamer') {
+      $('div.message_holder').append(
+        '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-danger text-white rounded"  ><b style="color: #000;">' +
+        'Стример: ' + '</b>' + msg.message + 
+        '<a href="//' + msg.website + '" target="_blank"><div class="row align-items-center"><div class="col"><div class="container">' +
+        '<div class="row align-items-center"><div class="col">' +
+        '<img class="card-img-top" src=' + msg.image + '>' +
+        '</div><div class="col">' +
+        '<h5 class="card-title">' + msg.title + '</h5>' +
+        '</div></div></div></div></a></div>')
+    } else {
+      $('div.message_holder').append(
+        '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-info text-white rounded message_url"><b style="color: #000;">' +
+        msg.id + ': </b>' + msg.message +
+        '<a href="//' + msg.website + '" target="_blank"><div class="row align-items-center"><div class="col"><div class="container">' +
+        '<div class="row align-items-center"><div class="col">' +
+        '<img class="card-img-top" src=' + msg.image + '>' +
+        '</div><div class="col">' +
+        '<h5 class="card-title">' + msg.title + '</h5>' +
+        '</div></div></div></div></a></div>')
+    }
+
+
+    if (output.selectionStart == output.selectionEnd) {
+      output.scrollTop = output.scrollHeight;
+    }
+  }
+  else if (typeof msg.message !== 'undefined' && msg.message != '' && msg.message != ' ') {
+    var output = document.getElementById("output");
+    if (msg.id == 'streamer') {
+      $('div.message_holder').append(
+        '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-danger text-white rounded"  ><b style="color: #000;">' +
+        'Стример: ' + '</b>' + msg.message + '</div>')
+    } else {
+      $('div.message_holder').append(
+        '<div style="word-wrap: break-word;" class="p-3 mb-2 bg-info text-white rounded message_url"><b style="color: #000;">' +
+        msg.id + ': </b>' + msg.message + '</div>')
+    }
+
+
+    if (output.selectionStart == output.selectionEnd) {
+      output.scrollTop = output.scrollHeight;
+    }
+  }
+})
+
+
+Temp = 0;
+$(document).ready(function () {
+  function scrollToBottom() {
+    $('#output').scrollTop($('#output')[0].scrollHeight);
+  }
+  $("#output").change(function () {
+    scrollToBottom();
+  });
+  $("#btn").click(function () {
+    $("#supa").hide();
+    $("#Create").toggle();
+    $("#supa2").show();
+    $("#btn").removeClass('blink').html();
+    if (Temp == 0) {
+      Temp = 1;
+    } else {
+      Temp = 0;
+    }
+    $('#output').scrollTop($('#output')[0].scrollHeight);
+  });
+});
+
+$("#supa").hide();
+$("#Create").hide();
